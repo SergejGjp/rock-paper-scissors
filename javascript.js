f751d89277c9ffc136404f3d38c8a7266bc0adc2
Sergej <gorbatschewsergej@gmail.com>
2023-09-15 23:54:02 +0900

    Add pseudocode Explaned the game in the javascript file.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//Rock Paper Scissors Game in Browser Console.
+//
+//The code prompts one option from the user "rock, paper or scissors", the input is case-insensitive
+//The computer choice is a random choice out of the three options.
+//A single game is using two parameters, the user choice and the random computer choice. The function checks if the winning conditions is met and then returns a string.
+//The game consists out of 5 rounds and tracks score, in the end the winner is announced.
+
